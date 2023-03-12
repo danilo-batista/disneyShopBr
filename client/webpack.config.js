bd603f6cd54d94082867c28178fbfeccb565fa55
Danilo Batista <dbatistadsn@gmail.com>
2023-03-12 16:03:02 -0300

    feat: install 'html-webpack-plugin' to generate html import template.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,5 +1,6 @@
 /* Importação para rpadronizar a localização dos arquivos. */
 const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -11,6 +12,11 @@ module.exports = {
     resolve: {
         extensions: ['.js', '.jsx']
     },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public', 'index.html')
+        })
+    ],
     module: {
         rules: [
             {
