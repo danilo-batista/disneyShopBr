093fc9cb1463815c4765e7302742897999ac20a8
Danilo Batista <dbatistadsn@gmail.com>
2023-03-12 17:35:07 -0300

    fix: update 'render' method with new specifications from 'react.js 18'.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,10 @@
 export function App() {
-    return <div>
-        <cite>“We keep moving forward, opening up new doors and<br />
-            doing new things, because we’re curious...<br />
-            and curiosity keeps leading us down new paths."<br />
-            <strong><sub>Walt Disney</sub></strong></cite>
-    </div>;
+    return (
+        <div>
+            <cite>“We keep moving forward, opening up new doors and<br />
+                doing new things, because we’re curious...<br />
+                and curiosity keeps leading us down new paths."<br />
+                <strong><sub>Walt Disney</sub></strong></cite>
+        </div>
+    );
 }
